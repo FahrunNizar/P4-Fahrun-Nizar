6de7c74c852087fc89b0e92e2a5859368af7e291
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:32:18 -0700

    Create jobsheet 1 C++.cpp
    Menampilkan array satu dimensi(Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa)

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		cout<<"nama:fahrun nizar"<<endl;
+	cout<<"nim:F1B019051"<<endl;
+	cout<<"kelompok:11"<<endl;
+
+cout<<"Fahrun Nizar-F1B019051\n\n";
+int nim[5]= {1,9,0,5,1};
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+}
+
+
+
+
+
