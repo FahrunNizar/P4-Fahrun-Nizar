6180a6f9a33b21ca301fb974d3af75934bec2983
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:27:55 -0700

    Create jobsheet 1 C.c
    Menampilkan array satu dimensi(Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa)

---
+++ jobsheet 1 C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+printf ("nama : fahrun nizar\n");
+printf ("nim : F1B019051\n ");
+printf ("kelompok : 11\n");
+
+char* nim_ray[6]={"f1b019111",
+"f1b099051","f1b019044",
+"f1b019054","f1b019041","f1b019021"};
+	printf("nim mahasiswa ke- 1 = %s\n",nim_ray[0]);
+	printf("nim mahasiswa ke- 2 = %s\n",nim_ray[1]);
+	printf("nim mahasiswa ke- 3 = %s\n",nim_ray[2]);
+	printf("nim mahasiswa ke- 4 = %s\n",nim_ray[3]);
+	printf("nim mahasiswa ke- 5 = %s\n",nim_ray[4]);
+	printf("nim mahasiswa ke- 6 = %s\n",nim_ray[5]);
+}
+
