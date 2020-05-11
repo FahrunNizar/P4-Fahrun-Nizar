169871d2a5694a0a33545983f215f561c745d1a9
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:55:48 -0700

    Add files via upload
    Mengisi elemen array menggunakan perulangan (Buatlah program mengisinilai array denganperulangandengan index array dan bataspengulanganmenggunakan NIM Terakhirsetiapmahasiswa)meggunakan bahasa C++.

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main (){
+cout<<"nama:fahrun nizar"<<endl;
+	cout<<"nim:F1B019051"<<endl;
+	cout<<"kelompok:11"<<endl;
+
+int nim [1];
+for (int i = 0; i<= 1; i++){
+cout<< "Masukkan nimke-= " << i+1;
+cin>>nim[i];
+}
+}
+
+
