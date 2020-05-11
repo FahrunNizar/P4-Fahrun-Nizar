b7f44209c34aade2a1af84c47534d3d66cb9d094
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 12:00:05 -0700

    Add files via upload
    Array sebagai karakter ( Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa)menggunakan bahasa C.

---
+++ jobsheet 9 C.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+int main()
+{
+	
+printf ("nama : fahrun nizar\n");
+printf ("nim : F1B019051\n ");
+printf ("kelompok : 11\n");
+
+int i;
+char nama[5]={'K','U','T','U'};
+char nama2[6]="KUTU";
+printf("Array per Karakter = ");
+for(i=0;i<=4;i++)
+{
+printf("%c",nama[i]);
+}
+printf("\n"); printf("Array string =%s\n\n",nama2);
+return 0;
+}
+
