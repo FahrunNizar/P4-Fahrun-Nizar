3f8e83948e6e7eebed785e28f84a83d8bb46e897
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:50:05 -0700

    Add files via upload
    Perkalian matriks Dua dimensi(Buatlah program perkalian matriks duadimensi denganmenggunakan NIM dua orang mahasiswa) menggunakan bahasa C.

---
+++ jobsheet 5 C.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+
+main (){
+printf ("nama : fahrun nizar\n");
+printf ("nim : F1B019051\n ");
+printf ("kelompok : 11\n");
+
+int nim1 [2][2];
+int nim2 [2][2];
+int hasil [2][2];
+int i, j;
+printf ("PERKALIAN NIM\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &nim1[i][j]);
+}
+}
+printf ("\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &nim2[i][j]);
+}
+}
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+hasil [i][j]=nim1[i][0]*nim2[0][j]+nim1[i][1]*nim2[1][j];
+printf ("%d ", hasil[i][j]);
+}
+printf ("\n");
+}
+getch();
+}
+
