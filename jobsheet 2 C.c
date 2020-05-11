d3f12f7ad20507657f86964c04fdf02744e96636
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:35:39 -0700

    Create jobsheet 2 C.c
    Menampilkan array 2 dimensi(Buatlah array dua dimensi yang menampilkan NIM dari mahasiswa yang berbeda  )

---
+++ jobsheet 2 C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+printf ("nama : fahrun nizar\n");
+printf ("nim : F1B019051\n ");
+printf ("kelompok : 11\n");
+
+int x [5];
+printf("masukan 2 angka akhiran nim anda\n\n");
+printf("nim nilai 1: ");
+scanf("%i",&x[0]);
+printf("nim nilai 2: ");
+scanf("%i",&x[1]);
+printf("nim nilai 3: ");
+scanf("%i",&x[2]);
+scanf("%i",&x[4]);
+printf("\n");
+printf("nim ke-1 =%i\n",x[0]);
+printf("nim ke-2 =%i\n",x[1]);
+printf("nim ke-3 =%i\n",x[2]);
+return 0;
+}
+
+
