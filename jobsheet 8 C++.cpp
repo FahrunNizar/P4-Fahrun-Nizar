512d7c8afc1c4ada4df75886646c634b7339fac2
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:58:26 -0700

    Add files via upload
    Mengisi dan menampilkan elemen array menggunakan pengulangan(Buatlah program mengisi nilai array dan menampilkannya dengan pengulang menggunakan NIM terakhir mahasiswa)menggunakan bahasa C++.

---
+++ jobsheet 8 C++.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+using namespace std;
+int main ()
+{
+	cout<<"nama:fahrun nizar"<<endl;
+	cout<<"nim:F1B019051"<<endl;
+	cout<<"kelompok:11"<<endl;
+
+int x[3],i;
+cout<<"masukkan NIM terakhir Anda"<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"Masukkan nim "<<i+1<<" : ";
+cin>>x[i];
+}
+cout<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"nim ke- "<<i+1<<" = "<<x[i]<<endl;
+} cout<<endl;
+
+ return 0;
+}
+
