8aa2525e4c53b9990ecbac00761a835860797f4d
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 12:00:48 -0700

    Add files via upload
    Array sebagai karakter ( Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa)menggunakan bahasa C++.

---
+++ jobsheet 9 C++.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+using namespace std;
+int main(){
+	cout<<"nama:fahrun nizar"<<endl;
+	cout<<"nim:F1B019051"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+
+int i;
+char nama[5]={'A','I','R',};
+char nama2[6]="AIR";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
+
