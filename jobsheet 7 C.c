d120c149992645290f7cdfad99dc1f2a07dcdb76
FahrunNizar <64617011+FahrunNizar@users.noreply.github.com>
2020-05-11 11:55:03 -0700

    Add files via upload
    Mengisi elemen array menggunakan perulangan (Buatlah program mengisinilai array denganperulangandengan index array dan bataspengulanganmenggunakan NIM Terakhirsetiapmahasiswa)meggunakan bahasa C.

---
+++ jobsheet 7 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main (){
+printf ("nama : fahrun nizar\n");
+printf ("nim : F1B019051\n ");
+printf ("kelompok : 11\n");
+
+int nim [1], i;
+for (i = 0; i<= 1; i++){
+printf ("Masukkan nimke-%i= ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
+
